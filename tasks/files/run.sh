cb623cfbb3fc7350da4eee30e162f6b6d8aa73d7
Ahmed Bayoumy <abayoumy@outlook.com>
2023-07-05 12:07:13 +0300

    add epel-release for dnf destro

--- tasks/files/run.sh
+++ tasks/files/run.sh
@@ -3,7 +3,7 @@
 if ! [ -x "$(command -v ansible-pull)" ]; then 
     packagesNeeded='ansible git'
     if [ -x "$(command -v apt)" ]; then sudo apt update && sudo apt upgrade -y && sudo apt install $packagesNeeded -y
-    elif [ -x "$(command -v dnf)" ];     then sudo dnf update -y && sudo dnf install $packagesNeeded -y
+    elif [ -x "$(command -v dnf)" ]; then sudo dnf update -y && sudo dnf install -y epel-release && sudo dnf install $packagesNeeded -y
     elif [ -x "$(command -v zypper)" ];  then sudo zypper install $packagesNeeded
     else echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: $packagesNeeded">&2; 
     fi
