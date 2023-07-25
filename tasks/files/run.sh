79b8e1326f069693b3f8afdae7c4a9c785230eb1
Ahmed Bayoumy <abayoumy@outlook.com>
2023-07-25 11:32:11 +0300

    edit run.sh

--- tasks/files/run.sh
+++ tasks/files/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if ! [ -x "$(command -v ansible-pull)" ]; then 
-    packagesNeeded='ansible git'
+    packagesNeeded='ansible-core git'
     if [ -x "$(command -v apt)" ]; then sudo apt update && sudo apt upgrade -y && sudo apt install $packagesNeeded -y
     elif [ -x "$(command -v dnf)" ]; then sudo dnf update -y && sudo dnf install -y epel-release && sudo dnf install $packagesNeeded -y
     elif [ -x "$(command -v zypper)" ];  then sudo zypper install $packagesNeeded
