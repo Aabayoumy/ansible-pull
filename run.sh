cb9a9897cc448880b57946c8fd2e16e40f5d3a08
Ahmed Bayoumy <abayoumy@outlook.com>
2024-08-18 09:06:39 +0300

    add info task

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if ! [ -x "$(command -v ansible-pull)" ]; then 
+    packagesNeeded='ansible-core git'
+    if [ -x "$(command -v apt)" ]; then sudo apt update && sudo apt upgrade -y && sudo apt install $packagesNeeded -y
+    elif [ -x "$(command -v dnf)" ]; then sudo dnf update -y && sudo dnf install -y epel-release && sudo dnf install $packagesNeeded -y
+    elif [ -x "$(command -v zypper)" ];  then sudo zypper install $packagesNeeded
+    elif [ -x "$(command -v pacman)" ];  then sudo pacman -Sy $packagesNeeded  --noconfirm
+    else echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: $packagesNeeded">&2; 
+    fi
+fi
+ansible-galaxy install -r requirements.yml
+ansible-pull -U https://github.com/Aabayoumy/ansible-pull.git
+source ~/.bashrc
+
