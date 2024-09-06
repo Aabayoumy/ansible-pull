fba6bcf937083ed5e9c2e54ba347dabffdf99b04
Ahmed Bayoumy <abayoumy@outlook.com>
2024-09-06 20:50:49 +0300

    test run

--- run.sh
+++ run.sh
@@ -255,7 +255,7 @@ update_ansible_galaxy $ID
 
 
 __task "Running playbook ($USER)"; 
-_cmd "ansible-pull -U https://github.com/Aabayoumy/ansible-pull.git -C Pull-Test"
+ansible-pull -U https://github.com/Aabayoumy/ansible-pull.git -C Pull-Test
 _task_done
 
 
