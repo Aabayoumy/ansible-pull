71dfd77405e5e981dcd93bc527ede3bbeb035d8e
Ahmed Bayoumy <abayoumy@outlook.com>
2024-09-06 20:51:44 +0300

    move to main branch

--- run.sh
+++ run.sh
@@ -255,7 +255,7 @@ update_ansible_galaxy $ID
 
 
 __task "Running playbook ($USER)"; 
-ansible-pull -U https://github.com/Aabayoumy/ansible-pull.git -C Pull-Test
+ansible-pull -U https://github.com/Aabayoumy/ansible-pull.git
 _task_done
 
 
