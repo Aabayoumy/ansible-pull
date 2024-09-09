e013b87833f0810dd03de0132ae04d0640189908
Ahmed Bayoumy <abayoumy@outlook.com>
2024-09-09 10:14:23 +0300

    users

--- pre_tasks/files/dotfiles/.config/fish/config.fish
+++ pre_tasks/files/dotfiles/.config/fish/config.fish
@@ -20,26 +20,6 @@
 if not status --is-interactive
    exit
 end
-
-if status is-interactive
-    # Configure auto-attach/exit to your likings (default is off).
-    set ZELLIJ_AUTO_ATTACH true
-    set ZELLIJ_AUTO_EXIT true
-    eval (zellij setup --generate-auto-start fish | string collect)
-end
-if not set -q ZELLIJ
-    if test "$ZELLIJ_AUTO_ATTACH" = "true"
-        zellij attach -c
-    else
-        zellij
-    end
-
-    if test "$ZELLIJ_AUTO_EXIT" = "true"
-        kill $fish_pid
-    end
-end
-
-
 # Load private config
 if [ -f $HOME/.config/fish/private.fish ]
     source $HOME/.config/fish/private.fish
